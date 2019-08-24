54b14409baa0cc2b050619252487db68d5397d9b
Rahul Khanna <khanna.mailme@gmail.com>
2019-08-24 23:59:11 +0800

    update the handler event

--- generators/handler/index.js
+++ generators/handler/index.js
@@ -21,8 +21,8 @@ module.exports = class extends Generator {
       {
         type: 'input',
         name: 'name',
-        message: 'What is your handler endpoint name?',
-        default: this.config.get('last_endpoint') || 'endpoint',
+        message: 'What is your handler name?',
+        default: this.config.get('last_endpoint') || 'handler',
         validate: value => value !== undefined && value !== ''
       },
       {
@@ -32,11 +32,25 @@ module.exports = class extends Generator {
         default: this.config.get('last_apidesc') || 'description'
       },
       {
+        type: 'confirm',
+        name: 'handlerType',
+        message: 'Does your handler support HTTP Type? (y/N)',
+        default: this.config.get('last_httpHandlerType') || 0
+      },
+      {
+        type: 'confirm',
+        name: 'requiresMiddleware',
+        message: 'Does your handler requires default middleware? (y/N)',
+        default: this.config.get('last_handlerrequire') || 0,
+        when: answer => (answer.handlerType)
+      },
+      {
         type: 'list',
         name: 'method',
         message: 'Handler Method?',
         default: this.config.get('last_method') || 'post',
-        choices: ['get', 'post', 'put', 'delete']
+        choices: ['get', 'post', 'put', 'delete'],
+        when: answer => (answer.handlerType)
       }
     ]).then((answers) => {
       that.props = answers;
@@ -44,7 +58,8 @@ module.exports = class extends Generator {
       that.props.apibase = that.config.get('apibase') || 'api';
       that.props.apiversion = that.config.get('apiversion') || 'v1';
       that.props.endpoint = `${that.props.name}`;
-      that.props.upperMethod = that.props.method.toUpperCase();
+      that.props.httpHandlerType = `${that.props.handlerType}`;
+      that.props.requiresMiddleware = `${that.props.requiresMiddleware}`;
       that.props.filename = underscored(that.props.name).replace('_', '-');
       that.props.src = that.config.get('src') || 'src';
       that.props.apiPath = urlJoin(that.props.src, that.props.apibase);
@@ -79,6 +94,8 @@ module.exports = class extends Generator {
         this.config.set('last_method', that.props.method);
         this.config.set('apibase', that.props.apibase);
         this.config.set('apiversion', that.props.apiversion);
+        this.config.set('last_handlerType', that.props.httpHandlerType);
+        this.config.set('last_handlerrequire', that.props.requiresMiddleware);
       });
 
       done();
@@ -114,7 +131,7 @@ module.exports = class extends Generator {
     /**
      * Validation
      */
-    if (!this.fs.exists(dPath(`${apiPath}/${validationName}`))) {
+    if (this.props.httpHandlerType === 'true' && !this.fs.exists(dPath(`${apiPath}/${validationName}`))) { // eslint-disable-line
       // As of v3.0.1 We no longer use validation
       // copyTpl(tPath('_validation.ejs'), dPath(`${apiPath}/${validationName}`), props);
       copyTpl(tPath('_validator.ejs'), dPath(`${apiPath}/${validationName}`), props);
